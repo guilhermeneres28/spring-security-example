469c398eb4d054a1572d1aba9300658c1c816e6b
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-12-11 15:04:06 -0300

    Adicionando configurações para inicializar os filtros do spring security e iniciando configuração

---
+++ src/main/java/br/com/guilherme/springsecurity/demo/config/SucurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package br.com.guilherme.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SucurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
