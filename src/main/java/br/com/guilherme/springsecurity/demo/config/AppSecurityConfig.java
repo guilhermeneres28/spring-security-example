6a2a2081d671f38a1f002e6428165a1565efe32c
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-12-12 16:29:34 -0300

    Adicionando usuarios em memoria para autenticação

--- src/main/java/br/com/guilherme/springsecurity/demo/config/AppSecurityConfig.java
+++ src/main/java/br/com/guilherme/springsecurity/demo/config/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +12,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication().withUser(users.username("guilherme").password("test123").roles("DEV"));
     }
 }
